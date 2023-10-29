9542558dfdb1df7c04e49176163fb5aafb38210a
Sergei Prokofev <fogg8341@gmail.com>
2023-10-29 11:53:49 +0400

    did HWO4 without optional

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,41 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedNativeQueries({
+        @NamedNativeQuery(name = Meal.GET, query = "SELECT * FROM Meal " +
+                "WHERE id = :id AND user_id = :user_id",
+                resultClass = Meal.class),
+        @NamedNativeQuery(name = Meal.ALL_SORTED, query = "SELECT * FROM Meal WHERE user_id = :user_id ORDER BY date_time DESC",
+                resultClass = Meal.class),
+        @NamedNativeQuery(name = Meal.DELETE, query = "DELETE FROM Meal WHERE id = :id AND user_id = :user_id"),
+        @NamedNativeQuery(name = Meal.BETWEEN_HALF_OPEN, query = "SELECT * FROM meal WHERE user_id = :user_id  AND date_time >= :startDateTime " +
+                "AND date_time < :endDateTime ORDER BY date_time DESC")
+})
+@Entity
+@Table(name = "meal")
 public class Meal extends AbstractBaseEntity {
+    public static final String GET = "Meal.get";
+    public static final String ALL_SORTED = "Meal.sorted";
+    public static final String DELETE = "Meal.delete";
+    public static final String BETWEEN_HALF_OPEN = "Meal.betweenHalfOpen";
+    @Column(name = "date_time")
+    @NotNull
+    @NotBlank
     private LocalDateTime dateTime;
-
+    @Column(name = "description")
+    @NotNull
+    @NotBlank
     private String description;
-
+    @Column(name = "calories")
     private int calories;
-
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Meal() {
